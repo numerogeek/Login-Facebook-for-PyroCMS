253fadf69e3d3323ee4625564a90423421fc6423
Khalil TABBAL <khalil.tabbal@gmail.com>
2012-08-07 19:56:14 -0400

    Installation details modification

--- details.php
+++ details.php
@@ -9,105 +9,112 @@ class Module_login extends Module
         {
                 return array(
                     'name' => array(
-                        'en'          => 'Login',
-                        'fr'          => 'Login'
+                        'en' => 'Login',
+                        'fr' => 'Login'
                     ),
                     'description' => array(
-                        'en'       => 'This is a PyroCMS module login.',
-                        'fr'       => 'Module permettant la gestion de login Facebook'
+                        'en' => 'This is a PyroCMS module login.',
+                        'fr' => 'Module permettant la gestion de login Facebook'
                     ),
                     'frontend' => TRUE,
-                    'backend'  => TRUE
+                    'backend' => TRUE
                 );
         }
 
         public function install()
         {
-                
+                // check if Facebook Spark is installed (should be in system/sparks/facebook-sdk/0.0.1)
+                // todo not case sensitive
+                if (!is_dir('system/sparks/facebook-sdk/0.0.1'))
+                {
+                        return FALSE;
+                }
+
+                // add settings to config for your Facebook App
                 $settings = array(
                     'login_setting' => array(
-                        'slug'           => 'login_setting',
-                        'title'          => 'Facebook Login',
-                        'description'    => 'Used to redirect a user to a module after he has logged in with is Facebook',
-                        'default'        => '1',
-                        'value'          => '1',
-                        'type'           => 'select',
-                        '`options`'      => '1=Yes|0=No',
-                        'is_required'    => 1,
-                        'is_gui'         => 1,
-                        'module'         => 'login'
+                        'slug' => 'login_setting',
+                        'title' => 'Facebook Login',
+                        'description' => 'Used to redirect a user to a module after he has logged in with is Facebook',
+                        'default' => '1',
+                        'value' => '1',
+                        'type' => 'select',
+                        '`options`' => '1=Yes|0=No',
+                        'is_required' => 1,
+                        'is_gui' => 1,
+                        'module' => 'login'
                     ),
                     'login_redirect' => array(
-                        'slug'           => 'login_redirect',
-                        'title'          => 'Url to redirect to',
-                        'description'    => 'Permet de rediriger l\'utilisateur vers un module',
-                        'default'        => '',
-                        'value'          => '',
-                        'type'           => 'text',
-                        '`options`'      => '',
-                        'is_required'    => 1,
-                        'is_gui'         => 1,
-                        'module'         => 'login'
+                        'slug' => 'login_redirect',
+                        'title' => 'Url to redirect to',
+                        'description' => 'Permet de rediriger l\'utilisateur vers un module',
+                        'default' => '',
+                        'value' => '',
+                        'type' => 'text',
+                        '`options`' => '',
+                        'is_required' => 1,
+                        'is_gui' => 1,
+                        'module' => 'login'
                     ),
                     'login_fb_scope' => array(
-                        'slug'             => 'login_fb_scope',
-                        'title'            => 'Facebook scope',
-                        'description'      => 'Facebook scope parameters with comma-separated list of permissions." ',
-                        'default'          => '',
-                        'value'            => '',
-                        'type'             => 'text',
-                        '`options`'        => '',
-                        'is_required'      => 1,
-                        'is_gui'           => 1,
-                        'module'           => 'login'
+                        'slug' => 'login_fb_scope',
+                        'title' => 'Facebook scope',
+                        'description' => 'Facebook scope parameters with comma-separated list of permissions." ',
+                        'default' => '',
+                        'value' => '',
+                        'type' => 'text',
+                        '`options`' => '',
+                        'is_required' => 1,
+                        'is_gui' => 1,
+                        'module' => 'login'
                     ),
                     'login_fb_display' => array(
-                        'slug'           => 'login_fb_display',
-                        'title'          => 'Display facebook',
-                        'description'    => 'Facebook login display',
-                        'default'        => '',
-                        'value'          => '',
-                        'type'           => 'text',
-                        '`options`'      => '',
-                        'is_required'    => 1,
-                        'is_gui'         => 1,
-                        'module'         => 'login'
+                        'slug' => 'login_fb_display',
+                        'title' => 'Display facebook',
+                        'description' => 'Facebook login display',
+                        'default' => 'page', // more information available on Facebook Website : http://developers.facebook.com/docs/reference/dialogs/oauth/
+                        'value' => '',
+                        'type' => 'text',
+                        '`options`' => '',
+                        'is_required' => 1,
+                        'is_gui' => 1,
+                        'module' => 'login'
                     ),
                     'login_fb_appid' => array(
-                        'slug'               => 'login_fb_appid',
-                        'title'              => 'Facebook App id',
-                        'description'        => 'Your Facebook App ID',
-                        'default'            => '',
-                        'value'              => '',
-                        'type'               => 'text',
-                        '`options`'          => '',
-                        'is_required'        => 1,
-                        'is_gui'             => 1,
-                        'module'             => 'login'
+                        'slug' => 'login_fb_appid',
+                        'title' => 'Facebook App id',
+                        'description' => 'Your Facebook App ID',
+                        'default' => '',
+                        'value' => '',
+                        'type' => 'text',
+                        '`options`' => '',
+                        'is_required' => 1,
+                        'is_gui' => 1,
+                        'module' => 'login'
                     ),
                     'login_fb_appsecret' => array(
-                        'slug'                 => 'login_fb_appsecret',
-                        'title'                => 'Facebook App Secret',
-                        'description'          => 'Your Facebook App Secret',
-                        'default'              => '',
-                        'value'                => '',
-                        'type'                 => 'text',
-                        '`options`'            => '',
-                        'is_required'          => 1,
-                        'is_gui'               => 1,
-                        'module'               => 'login'
+                        'slug' => 'login_fb_appsecret',
+                        'title' => 'Facebook App Secret',
+                        'description' => 'Your Facebook App Secret',
+                        'default' => '',
+                        'value' => '',
+                        'type' => 'text',
+                        '`options`' => '',
+                        'is_required' => 1,
+                        'is_gui' => 1,
+                        'module' => 'login'
                     ),
                     'login_fb_redirecturl' => array(
-                        'slug'        => 'login_fb_redirecturl',
-                        'title'       => 'Facebook Redirect URI',
+                        'slug' => 'login_fb_redirecturl',
+                        'title' => 'Facebook Redirect URI',
                         'description' => 'Facebook redirect URI',
-                        'default'     => '',
-                        'value'       => '',
-                        'type'        => 'text',
-                        '`options`'   => '',
+                        'default' => '',
+                        'value' => '',
+                        'type' => 'text',
+                        '`options`' => '',
                         'is_required' => 1,
-                        'is_gui'      => 1,
-                        'module'      => 'login'
+                        'is_gui' => 1,
+                        'module' => 'login'
                     )
                 );
 
@@ -123,51 +130,49 @@ class Module_login extends Module
                         }
                 }
 
-
-               
-                        $facebook_id=$this->streams->fields->get_field_assignments('facebook_id', 'users');
-                        if (empty($facebook_id))
-                        {
-                                $fields = array(
-                                        'name' => 'Facebook_id',
-                                        'slug' => 'facebook_id',
-                                        'namespace' => 'users',
-                                        'type' => 'text',
-                                        'assign'=>'profiles',
-                                        'extra' => array('max_length' => 200)
-                                );
-                                $this->streams->fields->add_field($fields);
-                                return true;
-                        }
+                // check if there is a field facebook_id in users default_profiles
+                $facebook_id = $this->streams->fields->get_field_assignments('facebook_id', 'users');
+                if (empty($facebook_id))
+                {
+                        $fields = array(
+                            'name' => 'Facebook_id',
+                            'slug' => 'facebook_id',
+                            'namespace' => 'users',
+                            'type' => 'text',
+                            'assign' => 'profiles',
+                            'extra' => array('max_length' => 200)
+                        );
+                        $this->streams->fields->add_field($fields);
                         return true;
+                }
+                return true;
         }
 
         public function uninstall()
         {
-               
 
-                $facebook_id=$this->streams->fields->get_field_assignments('facebook_id', 'users');
+
+                $facebook_id = $this->streams->fields->get_field_assignments('facebook_id', 'users');
                 if (empty($facebook_id))
                 {
                         $this->streams->fields->delete_field('facebook_id', 'users');
                 }
-                
+
                 $this->db->delete('settings', array('module' => 'login'));
                 {
                         return TRUE;
                 }
-
         }
 
         public function upgrade($old_version)
         {
-                
+
                 return TRUE;
         }
 
         public function help()
         {
-               
+
                 return "No documentation has been added for this module.<br />Contact the module developer for assistance.";
         }
 
